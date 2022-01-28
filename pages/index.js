bd323d7bb59f042801e4e6982fd48e6c92ce7793
Brenda Ramos Bezerra <brendaramos.bezerra@gmail.com>
2022-01-27 21:54:53 -0300

    added the usestate function on the textfield and the userouter function on the submit button, also implemented the chat page along with the _app file that holds the global style

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
-
-
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-            * {
-                margin: 0;
-                padding: 0;
-                box-sizing: border-box;
-                list-style: none;
-            }
-
-            body {
-                font-family: 'Open Sans', sans-serif;
-            }
-
-            /* App fit Height */ 
-            html, body, #__next {
-                min-height: 100vh;
-                display: flex;
-                flex: 1;
-            }
-
-            #__next {
-                flex: 1;
-            }
-            #__next > * {
-                flex: 1;
-            }
-            /* ./App fit Height */ 
-
-        `}</style>
-    );
-
-}
 
 function Title(props) {
     console.log(props);
@@ -69,11 +36,12 @@ function Title(props) {
 
 
 export default function PaginaInicial() {
-    const username = 'breramos';
+    //const username = 'breramos';
+    const [username, setUsername] = React.useState('breramos'); //a palavra use é um padrão do react para interceptar coisas, é um gancho tipo um cookie
+    const roteamento = useRouter();
   
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'right',
@@ -100,6 +68,11 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function  (infosdoEvento) {
+                infosdoEvento.preventDefault();
+                roteamento.push('/chat');
+                //window.location.href = '/chat';  o next é quem faz o roteamento de páginas e esse é o jeito tradicional do navegador de fazer isso
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -111,6 +84,16 @@ export default function PaginaInicial() {
               </Text>
   
               <TextField
+                value={username}
+                onChange={ function (event) {
+                  console.log('usuario digitou', event.target.value);
+                  //Onde está o valor?
+                  const valor = event.target.value;
+                  //Trocar o valor da variável
+                  //através do React e avisar quem precisa
+                  setUsername(valor);
+                
+                }}
                 fullWidth
                 textFieldColors={{
                   neutral: {
