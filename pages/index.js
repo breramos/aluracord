2d8ce8d26e97ea0a7756f3b9dda55accf38ce967
Brenda Ramos Bezerra <brendaramos.bezerra@gmail.com>
2022-01-29 22:41:09 -0300

    Made some extra changes regarding the stylesheet

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,15 @@ export default function PaginaInicial() {
     //const username = 'breramos';
     const [username, setUsername] = React.useState('breramos'); //a palavra use é um padrão do react para interceptar coisas, é um gancho tipo um cookie
     const roteamento = useRouter();
-  
+    const dados = fetch('https://api.github.com/users/breramos');
+    fetch('https://api.github.com/users/breramos')
+    .then(function(respostaDoServidor) {
+        return respostaDoServidor.json ()
+    })
+    .then (function(respostaConvertida) {
+
+    })
+
     return (
       <>
         <Box
@@ -61,8 +69,8 @@ export default function PaginaInicial() {
               },
               width: '100%', maxWidth: '700px',
               borderRadius: '5px', padding: '32px', margin: '16px',
-              boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-              backgroundColor: appConfig.theme.colors.neutrals[200],
+              boxShadow: '0 2px 10px 0 rgb(0 0 0 / 10%)',
+              backgroundColor: appConfig.theme.colors.neutrals[300],  //#9AA5B1 referente ao 300
             }}
           >
             {/* Formulário */}
@@ -71,17 +79,18 @@ export default function PaginaInicial() {
               onSubmit={function  (infosdoEvento) {
                 infosdoEvento.preventDefault();
                 roteamento.push('/chat');
-                //window.location.href = '/chat';  o next é quem faz o roteamento de páginas e esse é o jeito tradicional do navegador de fazer isso
+                //window.location.href = '/chat';  o next é quem faz o roteamento de páginas e esse é o jeito tradicional do navegador de fazer isso. Cor 600 = #29333D
               }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-                width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+                width: { xs: '100%', sm: '60%' }, textAlign: 'center', marginBottom: '0px',
               }}
             >
               <Title tag="h2">Boas vindas de volta!</Title>
-              <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[400] }}>
+                {appConfig.h2}
+              <Text variant="body3" styleSheet={{ marginBottom: '32px', marginTop: '10px', fontSize: '18px', color: appConfig.theme.colors.neutrals[600]}}> 
                 {appConfig.name}
-              </Text>
+              </Text>  
   
               <TextField
                 value={username}
@@ -127,9 +136,9 @@ export default function PaginaInicial() {
                 alignItems: 'center',
                 maxWidth: '200px',
                 padding: '16px',
-                backgroundColor: appConfig.theme.colors.neutrals[800],
-                border: '1px solid',
-                borderColor: appConfig.theme.colors.neutrals[999],
+                backgroundColor: appConfig.theme.colors.neutrals[300],
+                //border: '1px solid',
+                //borderColor: appConfig.theme.colors.neutrals[100],
                 borderRadius: '10px',
                 flex: 1,
                 minHeight: '240px',
